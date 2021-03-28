6cf63a14f91c111d844b6b0b8ab24bd8b2a7562d
ctkrause <ckrause2@madisoncollege.edu>
2021-03-28 13:02:34 -0500

    updated pojo to include id hash and equals

--- src/main/java/matc/team/controller/TapResults.java
+++ src/main/java/matc/team/controller/TapResults.java
@@ -1,18 +1,23 @@
 package matc.team.controller;
 
+import matc.team.entity.Tappers;
+import matc.team.persistence.TappersDao;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/whatsontap")
 public class TapResults {
+    TappersDao dao = TappersDao.getInstance();
     // The Java method will process HTTP GET requests
     @GET
     // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getName() {
+    public List<Tappers> list() {
 
     }
 }
