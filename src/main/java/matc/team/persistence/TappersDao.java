07b14a6859f251a208f69204cc3242b9df216b8a
ctkrause <ckrause2@madisoncollege.edu>
2021-03-28 13:07:00 -0500

    update dao

--- src/main/java/matc/team/persistence/TappersDao.java
+++ src/main/java/matc/team/persistence/TappersDao.java
@@ -13,6 +13,16 @@ public class TappersDao{
     private List<Tappers> tappersList = new ArrayList<Tappers>();
     private static TappersDao instance;
 
+    private TappersDao() {
+
+    }
+
+    public static TappersDao getInstance() {
+        if (instance == null) {
+            instance = new TappersDao();
+        }
+    }
+
     public TappersDao() {
         tappersList.add(new Tappers(1, "Test Beer", "Test Brewery", "Testaurant",
                 "This is a test"));
