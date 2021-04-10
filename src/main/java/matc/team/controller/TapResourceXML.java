297e9cdb736c63349acd8a7dc07d10c44dd6f532
jholmquest <jpholmquest@gmail.com>
2021-04-10 13:13:43 -0500

    added class for xml responses

---
+++ src/main/java/matc/team/controller/TapResourceXML.java
@@ -0,0 +1,15 @@
+package matc.team.controller;
+
+import matc.team.entity.Tappers;
+import matc.team.persistence.TappersDao;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@Path("/tapsXML")
+public class TapResourceXML {
+}
